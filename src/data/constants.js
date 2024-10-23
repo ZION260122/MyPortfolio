1b6c5facc1d7b84f47ddc2b3ab4058f6284a54a4
utkarsh <98471140+ZION260122@users.noreply.github.com>
2024-10-23 22:45:19 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a dynamic and adaptable professional, driven by a passion for continuous learning and excellence. Committed to delivering top-tier results, I approach every challenge with enthusiasm and a growth mindset, ready to contribute meaningfully and achieve remarkable outcomes.",
   github: "https://github.com/ZION260122",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/drive/quota",
   linkedin: "https://www.linkedin.com/in/utkarsh-vashishtha/",
   insta: "https://www.instagram.com/utkar_sh3038/",
 };
